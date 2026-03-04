38abb88d1dee25bc923930e6a36059966f4bea3f
x-sss-x <manu48617@gmail.com>
2026-03-04 14:03:21 +0530

    Enhance styling and functionality in the editor components. Updated color variables in CSS, added double quote style in JSON, and improved the document editor with a new toolbar layout and basic nodes plugin integration.

--- src/components/form-doc-editor.tsx
+++ src/components/form-doc-editor.tsx
@@ -1,51 +1,59 @@
 "use client";
 
 import type { Value } from "platejs";
-import {
-  BoldPlugin,
-  ItalicPlugin,
-  UnderlinePlugin,
-} from "@platejs/basic-nodes/react";
 import { Plate, usePlateEditor } from "platejs/react";
 
 import { Editor, EditorContainer } from "@/components/ui/editor";
 import { FixedToolbar } from "@/components/ui/fixed-toolbar";
 import { MarkToolbarButton } from "@/components/ui/mark-toolbar-button";
- 
+import { BasicNodesKit } from "./editor/plugins/basic-nodes-kit";
+import { ToolbarButton, ToolbarGroup } from "./ui/toolbar";
 
 const initialValue: Value = [
   {
-    type: "p",
-    children: [
-      { text: "Hello! Try out the " },
-      { text: "bold", bold: true },
-      { text: ", " },
-      { text: "italic", italic: true },
-      { text: ", and " },
-      { text: "underline", underline: true },
-      { text: " formatting." },
-    ],
+    type: "h4",
+    children: [{ text: "INS Format 23" }],
   },
 ];
 
 export default function FormDocEditor() {
   const editor = usePlateEditor({
-    plugins: [BoldPlugin, ItalicPlugin, UnderlinePlugin], // Add the mark plugins
+    plugins: BasicNodesKit, // Add the mark plugins
     value: initialValue, // Set initial content
-  }); 
+  });
 
   return (
     <Plate editor={editor}>
-      <FixedToolbar className="justify-start rounded-t-lg">
-        <MarkToolbarButton nodeType="bold" tooltip="Bold (⌘+B)">
-          B
-        </MarkToolbarButton>
-        <MarkToolbarButton nodeType="italic" tooltip="Italic (⌘+I)">
-          I
-        </MarkToolbarButton>
-        <MarkToolbarButton nodeType="underline" tooltip="Underline (⌘+U)">
-          U
-        </MarkToolbarButton>
+      <FixedToolbar className="justify-center gap-4 rounded-t-lg">
+        <ToolbarGroup>
+          <MarkToolbarButton nodeType="bold" tooltip="Bold (⌘+B)">
+            B
+          </MarkToolbarButton>
+          <MarkToolbarButton nodeType="italic" tooltip="Italic (⌘+I)">
+            I
+          </MarkToolbarButton>
+          <MarkToolbarButton nodeType="underline" tooltip="Underline (⌘+U)">
+            U
+          </MarkToolbarButton>
+        </ToolbarGroup>
+        <ToolbarButton
+          onClick={() => editor.tf.toggleBlock("h2")}
+          tooltip="Heading 2 (⌘+⌥+2)"
+        >
+          H2
+        </ToolbarButton>
+        <ToolbarButton
+          onClick={() => editor.tf.toggleBlock("h3")}
+          tooltip="Heading 3 (⌘+⌥+3)"
+        >
+          H3
+        </ToolbarButton>
+        <ToolbarButton
+          onClick={() => editor.tf.toggleBlock("h4")}
+          tooltip="Heading 4 (⌘+⌥+4)"
+        >
+          H4
+        </ToolbarButton>
       </FixedToolbar>
       {/* Provides editor context */}
       <EditorContainer className="h-svh">
