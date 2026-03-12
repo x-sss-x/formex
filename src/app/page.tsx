1478bffdd52649ae7295e3c5dd631f08f12b4af0
x-sss-x <manu48617@gmail.com>
2026-03-12 18:51:45 +0530

    Update Home component to include navigation links for "Students" and reposition the NewTemplateButton within a new header layout for improved accessibility and organization.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import NewTemplateButton from "@/components/template/new-template-button";
 import { TemplateList } from "@/components/template/template-list";
 
@@ -6,7 +7,11 @@ export default function Home() {
     <main className="flex flex-col min-h-screen w-full">
       <header className="fixed top-0 h-14 border-b bg-background w-full px-6 flex justify-between items-center">
         <h1 className="text-lg font-bold font-mono">Formex</h1>
-        <NewTemplateButton />
+
+        <div className="space-x-6 [&>a]:text-sm [&>a]:font-semibold [&>a]:hover:underline">
+          <Link href={"/students"}>Students</Link>
+          <NewTemplateButton />
+        </div>
       </header>
       <div className="pt-14">
         <TemplateList />
